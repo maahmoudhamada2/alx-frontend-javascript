7eb59a767c23875fd278c90e43f3596ceb0a30c4
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-10-08 13:34:19 +0300

    Re-submit: 0x02-ES6_classes --> 4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newValue) {
+    this._amount = newValue;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newValue) {
+    this._currency = newValue;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
