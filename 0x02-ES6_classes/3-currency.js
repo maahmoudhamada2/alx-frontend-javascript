7aa4ce4643538b9516bb83e032704a4e24aff83f
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-10-27 16:55:36 +0300

    Re-submit: 0x02-ES6_classes --> 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newValue) {
+    this._code = newValue;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newValue) {
+    this._name = newValue;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
