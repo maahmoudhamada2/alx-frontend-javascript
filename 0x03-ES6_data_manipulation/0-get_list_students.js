e1b3e853a0ffaefb60d95dd7221b10b5bd1b0767
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-10-29 00:11:15 +0300

    Submit: 0x03-ES6 --> 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: 1, fileName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, fileName: 'James', location: 'Columbia' },
+    { id: 5, fileName: 'Serena', location: 'San Francisco' },
+  ];
+}
